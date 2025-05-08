40d468cfb49d9510607a5c24f8e46d4538f5bce8
Iam-frog <dcare7276@gmail.com>
2025-05-08 18:45:29 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-HEROKU = True  #
 
-# 
-if HEROKU:
-    from os import environ
-
-    Bot_token = environ["Bot_token"]
-    ARQ_API_KEY = environ["ARQ_API_KEY"]
-
-# 
-if not HEROKU:
     Bot_token = "7266490468:AAEsPDnsQ3bWdWNCX4rFMqJXNacnf6zKqlY"
     ARQ_API_KEY = "BOCULW-SEMUAC-MGWSPF-RYCMCB-ARQ"
